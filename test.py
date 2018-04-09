eca9fe7bc62b186e001da8c5635ba4ad73018848
Naomi Navkal <navkal@hotmail.com>
2018-04-09 18:22:59 -0400

    initial

--- test.py
+++ test.py
@@ -2,8 +2,9 @@
 
 import requests
 
-for i in range( 1, 10 ):
-
+for i in range( 1, 2 ):
+    '''
     gateway_rsp = requests.post( 'http://192.168.1.195:8000/bg.php', data={ 'caller': 'Python' } )
-
+    '''
+    gateway_rsp = requests.post( 'http://localhost:8000/bg.php', data={ 'caller': 'Python' } )
     print( '%2d:'%i, gateway_rsp.status_code, gateway_rsp.reason, gateway_rsp.text )
