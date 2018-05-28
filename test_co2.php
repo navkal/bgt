4b17ebc6c50e4c9bcce71518a6d1aa75bab0c57b
Naomi Navkal <navkal@hotmail.com>
2018-05-28 17:25:13 -0400

    test scripts

--- test_co2.php
+++ test_co2.php
@@ -1,9 +1,9 @@
 <?php
   // Copyright 2018 BACnet Gateway.  All rights reserved.
 
-  $sCsvFilename = 'test_electricity.csv';
-  $sInstanceName = 'Feeder';
-  $sInstanceValue = 'Meter Reading';
+  $sCsvFilename = 'test_co2.csv';
+  $sInstanceName = 'Location';
+  $sInstanceValue = 'CO2 Level';
 
   // Read CSV file containing list instances
   $file = fopen( $sCsvFilename, 'r' );
