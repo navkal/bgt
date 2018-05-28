fc50ae4c8f0f9d360ca26e5afe7000947778a72c
Naomi Navkal <navkal@hotmail.com>
2018-05-28 18:38:40 -0400

    test scripts

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   {
     $_SESSION['bgt'] =
       [
-        'host' => isset( $_REQUEST['host'] ) ? $_REQUEST['host'] : 'localhost',
+        'host' => isset( $_REQUEST['host'] ) ? $_REQUEST['host'] : '192.168.1.186',
         'port' => isset( $_REQUEST['port'] ) ? $_REQUEST['port'] : '8000'
       ];
   }
