0766fbe9a88f263dae3ef7d2901057c4a88a5949
Naomi Navkal <navkal@hotmail.com>
2018-05-29 10:50:16 -0400

    test scripts

--- test_electricity.py
+++ test_electricity.py
@@ -1,32 +1,38 @@
 # Copyright 2018 BACnet Gateway.  All rights reserved.
 
-import argparse
-import pandas as pd
-from bacnet_gateway_requests import get_value_and_units
+try:
+    import argparse
+    import pandas as pd
+    from bacnet_gateway_requests import get_value_and_units
 
-# Get hostname and port of BACnet Gateway
-parser = argparse.ArgumentParser( description='Test BACnet Gateway', add_help=False )
-parser.add_argument( '-h', dest='hostname' )
-parser.add_argument( '-p', dest='port' )
-args = parser.parse_args()
+    # Get hostname and port of BACnet Gateway
+    parser = argparse.ArgumentParser( description='Test BACnet Gateway', add_help=False )
+    parser.add_argument( '-h', dest='hostname' )
+    parser.add_argument( '-p', dest='port' )
+    args = parser.parse_args()
 
-# Read spreadsheet into a dataframe.
-# Each row contains the following:
-#   - Feeder
-#   - Instance ID of electric meter
-df = pd.read_csv( 'test_electricity.csv' )
+    # Read spreadsheet into a dataframe.
+    # Each row contains the following:
+    #   - Feeder
+    #   - Instance ID of electric meter
+    df = pd.read_csv( 'test_electricity.csv' )
 
-# Output column headings
-print( 'Feeder,Meter,Units' )
+    # Output column headings
+    print( 'Feeder,Meter,Units' )
 
-# Iterate over the rows of the dataframe, getting meter readings for each feeder
-for index, row in df.iterrows():
-    # Retrieve data
-    value, units = get_value_and_units( 'ahs', row['Meter'], args.hostname, args.port )
+    # Iterate over the rows of the dataframe, getting meter readings for each feeder
+    for index, row in df.iterrows():
+        # Retrieve data
+        value, units = get_value_and_units( 'ahs', row['Meter'], args.hostname, args.port )
 
-    # Prepare to print
-    value = int( value ) if value else ''
-    units = units if units else ''
+        # Prepare to print
+        value = int( value ) if value else ''
+        units = units if units else ''
 
-    # Output CSV format
-    print( '{0},{1},{2}'.format( row['Feeder'], value, units ) )
+        # Output CSV format
+        print( '{0},{1},{2}'.format( row['Feeder'], value, units ) )
+
+except KeyboardInterrupt:
+    print( 'Bye' )
+    import sys
+    sys.exit()
