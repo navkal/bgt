6a7f9fe9a92b6d0ac11e783f3697f88ca3e11b46
Naomi Navkal <navkal@hotmail.com>
2018-05-30 16:04:03 -0400

    dashboard prototype

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   {
     $_SESSION['bgt'] =
       [
-        'host' => isset( $_REQUEST['host'] ) ? $_REQUEST['host'] : $_SERVER['SERVER_ADDR'],
+        'host' => isset( $_REQUEST['host'] ) ? $_REQUEST['host'] : ( $_SERVER['SERVER_ADDR'] == '::1' ? '127.0.0.1' : $_SERVER['SERVER_ADDR'] ),
         'port' => isset( $_REQUEST['port'] ) ? $_REQUEST['port'] : '8000'
       ];
   }
