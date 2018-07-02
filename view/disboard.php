23a9239a62415c5a3a143945e2b9425ea6017431
Naomi Navkal <navkal@hotmail.com>
2018-07-02 13:49:08 -0400

    bar graph

---
+++ view/disboard.php
@@ -0,0 +1,29 @@
+<?php
+  // Copyright 2018 BACnet Gateway.  All rights reserved.
+
+  $g_sCsvFilename = 'csv/dashboard.csv';
+  $g_sFirstColName = 'Facility';
+  $g_aColNames =
+    [
+      [
+        'value_col_name' => 'Power',
+        'units_col_name' => 'Power Units',
+        'graph' =>
+          [
+            'graph_id' => 'power',
+            'delta' => false
+          ]
+      ],
+      [
+        'value_col_name' => 'Energy',
+        'units_col_name' => 'Energy Units',
+        'graph' =>
+          [
+            'graph_id' => 'energy',
+            'delta' => true
+          ]
+      ]
+    ];
+
+  include $_SERVER['DOCUMENT_ROOT'] . '/view/view_split_h.php';
+?>
