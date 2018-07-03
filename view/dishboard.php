9269d08af3fe316526fd9d77becb7b89bfd7409a
Naomi Navkal <navkal@hotmail.com>
2018-07-02 23:00:40 -0400

    bar graph

--- view/dishboard.php
+++ view/dishboard.php
@@ -4,26 +4,28 @@
   $g_sCsvFilename = 'csv/dashboard.csv';
   $g_sFirstColName = 'Facility';
   $g_aColNames =
+  [
     [
+      'value_col_name' => 'Power',
+      'units_col_name' => 'Power Units',
+      'graph' =>
       [
-        'value_col_name' => 'Power',
-        'units_col_name' => 'Power Units',
-        'graph' =>
-          [
-            'graph_id' => 'power',
-            'delta' => false
-          ]
-      ],
-      [
-        'value_col_name' => 'Energy',
-        'units_col_name' => 'Energy Units',
-        'graph' =>
-          [
-            'graph_id' => 'energy',
-            'delta' => true
-          ]
+        'graph_id' => 'power',
+        'delta' => false
       ]
-    ];
+    ],
+    [
+      'value_col_name' => 'Energy',
+      'units_col_name' => 'Energy Units',
+      'graph' =>
+      [
+        'graph_id' => 'energy',
+        'delta' => true
+      ]
+    ]
+  ];
 
-  include $_SERVER['DOCUMENT_ROOT'] . '/view/view_split_h.php';
+  $g_sLayoutMode = 'split';
+
+  include $_SERVER['DOCUMENT_ROOT'] . '/view/view.php';
 ?>
