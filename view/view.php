6ee7442ba6b3f8916c9b01f71f3dbc36493bebb8
Naomi Navkal <navkal@hotmail.com>
2018-07-23 23:53:07 -0400

    bar graph

--- view/view.php
+++ view/view.php
@@ -288,7 +288,7 @@
         </div>
         <div class="modal-footer">
           <div style="text-align:center;" >
-            <button type="button" id="baselinePickerFormSubmitProxy" class="btn btn-primary" onclick="$('#baselinePickerSubmitButton').click()" >Change Baseline</button>
+            <button type="button" id="baselinePickerFormSubmitProxy" class="btn btn-primary" onclick="$('#baselinePickerSubmitButton').click()" >Set Baseline</button>
             <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
           </div>
         </div>
