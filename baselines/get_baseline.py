f92c89077165a34f2e9cae4abcab4f15ba91c252
Naomi Navkal <navkal@hotmail.com>
2018-07-24 17:46:52 -0400

    bar graph

--- baselines/get_baseline.py
+++ baselines/get_baseline.py
@@ -34,19 +34,22 @@ if os.path.exists( db ):
     # Retrieve the timestamp
     cur.execute( 'SELECT id, timestamp FROM Timestamps WHERE timestamp=' + which_timestamp )
     timestamp_row = cur.fetchone()
-    timestamp_id = timestamp_row[0]
-    timestamp = timestamp_row[1]
 
-    # Retrieve values
-    cur.execute( 'SELECT row_label, value, units FROM Baselines WHERE ( csv_filename=? AND column_name=? AND timestamp_id=? )', ( args.csv_filename, args.column_name, timestamp_id ) )
-    value_rows = cur.fetchall()
-    values = {}
-    for value_row in value_rows:
-        values[value_row[0]] = { 'value': value_row[1], 'units': value_row[2] }
+    if timestamp_row:
+        timestamp_id = timestamp_row[0]
+        timestamp = timestamp_row[1]
 
-    values = collections.OrderedDict( sorted( values.items() ) )
-    baseline = { 'timestamp': timestamp, 'values': values }
-    baseline = collections.OrderedDict( sorted( baseline.items() ) )
+        # Retrieve values
+        cur.execute( 'SELECT row_label, value, units FROM Baselines WHERE ( csv_filename=? AND column_name=? AND timestamp_id=? )', ( args.csv_filename, args.column_name, timestamp_id ) )
+        value_rows = cur.fetchall()
+        values = {}
+        for value_row in value_rows:
+            values[value_row[0]] = { 'value': value_row[1], 'units': value_row[2] }
+
+        # Sort values and attach to baseline
+        values = collections.OrderedDict( sorted( values.items() ) )
+        baseline = { 'timestamp': timestamp, 'values': values }
+        baseline = collections.OrderedDict( sorted( baseline.items() ) )
 
 # Return timestamp and values
 print( json.dumps( baseline ) )
