151ee1e30aef5512f4f5b6e51c3e161687852291
Naomi Navkal <navkal@hotmail.com>
2018-07-26 14:48:33 -0400

    Set baseline timestamps in database for testing of Baseline Picker

--- baselines/doctor_baselines.py
+++ baselines/doctor_baselines.py
@@ -1,53 +1,26 @@
 # Copyright 2018 BACnet Gateway.  All rights reserved.
 
 import sqlite3
+import datetime
+import time
+
 
 # Connect to the database
 conn = sqlite3.connect( 'baselines.sqlite' )
 cur = conn.cursor()
 
-
+# Retrieve all timestamp IDs in descending order
 cur.execute( 'SELECT id FROM Timestamps ORDER BY id DESC' )
 rows = cur.fetchall()
 
+# Iterate over descending IDs, replacing timestamps with consecutive days, starting with today
+day = datetime.datetime.today()
 for row in rows:
+    timestamp = int( time.mktime( day.timetuple() ) ) * 1000
     id = row[0]
-    print( id )
+    cur.execute( 'UPDATE Timestamps SET timestamp=? WHERE id=?', ( timestamp, id ) )
+    print( id, datetime.datetime.fromtimestamp( timestamp/1000 ) )
+    day -= datetime.timedelta( days=1 )
 
-
-
-exit()
-
-# Determine which timestamp to retrieve
-if args.timestamp:
-    # Specified timestamp
-    which_timestamp = '( SELECT MIN( timestamp ) FROM ( SELECT timestamp from Timestamps WHERE timestamp>=' + args.timestamp + ' ) )'
-else:
-    # Default: Latest timestamp
-    which_timestamp = '( SELECT MAX( timestamp ) FROM Timestamps )'
-
-# Retrieve the timestamp
-timestamp_row = cur.fetchone()
-
-if timestamp_row:
-    timestamp_id = timestamp_row[0]
-    timestamp = timestamp_row[1]
-
-    # Retrieve values
-    cur.execute( 'SELECT row_label, value, units FROM Baselines WHERE ( csv_filename=? AND column_name=? AND timestamp_id=? )', ( args.csv_filename, args.column_name, timestamp_id ) )
-    value_rows = cur.fetchall()
-    values = {}
-    for value_row in value_rows:
-        values[value_row[0]] = { 'value': value_row[1], 'units': value_row[2] }
-
-
-    # Find earliest timestamp available for target graph
-    if values:
-        cur.execute( 'SELECT timestamp from Timestamps where id=( SELECT MIN( timestamp_id ) FROM Baselines WHERE ( csv_filename=? AND column_name=? ) )', ( args.csv_filename, args.column_name ) )
-        first_timestamp = cur.fetchone()[0]
-
-        cur.execute( 'SELECT timestamp from Timestamps where id=( SELECT MAX( timestamp_id ) FROM Baselines WHERE ( csv_filename=? AND column_name=? ) )', ( args.csv_filename, args.column_name ) )
-        last_timestamp = cur.fetchone()[0]
-
-
-
+# Commit changes
+conn.commit()
