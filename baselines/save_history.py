f40fdc4f39833e8d61ea416645cafc1305457072
Naomi Navkal <navkal@hotmail.com>
2018-07-30 15:58:12 -0400

    Load database with historic baselines

--- baselines/save_history.py
+++ baselines/save_history.py
@@ -8,6 +8,7 @@ import datetime
 
 parser = argparse.ArgumentParser( description='Save historic baseline values in database', add_help=False )
 parser.add_argument( '-h', dest='history_filename' )
+parser.add_argument( '-u', dest='units' )
 args = parser.parse_args()
 
 #
@@ -37,6 +38,9 @@ print( '\nbar map\n', bar_map )
 # Read history file into dataframe
 df = pd.read_csv( args.history_filename, index_col=[0] )
 
+# Replace n/a with empty strings
+df = df.fillna( '' )
+
 # Trim column headers
 df = df.rename( columns=lambda x: x.strip() )
 
@@ -45,14 +49,18 @@ df = df.sort_index()
 
 
 # Open the database
-conn, cur = common.open_db()
+conn, cur = common.open_db( remove=True )
 
 for index, row in df.iterrows():
     print( '=============' )
-    timestamp_id = common.save_timestamp( cur,  datetime.datetime.timestamp( index ) )
+    timestamp_id = common.save_timestamp( cur, datetime.datetime.timestamp( index ) )
     print( 'timestamp_id', timestamp_id )
-    # sr = df.loc[index]
-    # sr = sr.dropna()
-    # print( sr )
-    
+    sr = df.loc[index]
+    sr = sr.dropna()
+    for row_label, value in sr.iteritems():
+        print( row_label, 'val=<' + str(value) + '>' )
+        csv_filename = bar_map[row_label]['csv_filename']
+        column_name = bar_map[row_label]['column_name']
+        common.save_baseline_value( cur, csv_filename, column_name, row_label, value, args.units, timestamp_id )
+
 conn.commit()
