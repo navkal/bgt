efb504d42e695ea1838cb382561040bcc2d69697
Naomi Navkal <navkal@hotmail.com>
2018-07-30 11:59:59 -0400

    Set baseline timestamps in database for testing of Baseline Picker

--- baselines/save_history.py
+++ baselines/save_history.py
@@ -11,9 +11,22 @@ conn, cur = common.open_db( remove=True )
 
 
 # Get list of views for which we need baselines
-with open( 'baselines.csv', newline='' ) as csvfile:
+with open( 'baselines.csv', newline='' ) as baselines_file:
 
-    reader = csv.reader( csvfile )
+    baselines_reader = csv.reader( baselines_file )
 
-    for baselines_row in reader:
+    for baselines_row in baselines_reader:
         print( baselines_row )
+        view_filename = '../csv/' + baselines_row[0] + '.csv'
+        print( view_filename )
+
+        with open( view_filename, newline='' ) as view_file:
+
+            view_reader = csv.reader( view_file )
+
+            for view_row in view_reader:
+                print( view_row )
+
+
+
+
