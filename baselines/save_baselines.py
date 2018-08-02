f54a38d536c350e6140009275f075750bb4c1102
Naomi Navkal <navkal@hotmail.com>
2018-08-02 17:15:47 -0400

    Load database with historic baselines

--- baselines/save_baselines.py
+++ baselines/save_baselines.py
@@ -44,6 +44,47 @@ def save_baseline( csv_filename, column_name, oid_row ):
             break
 
 
+def report_missing_dates():
+
+    # Retrieve distinct timestamps from database
+    cur.execute('''
+        SELECT DISTINCT
+            timestamp
+        FROM Baselines
+            LEFT JOIN Timestamps ON Baselines.timestamp_id=Timestamps.id
+    ''')
+    timestamp_rows = cur.fetchall()
+
+    # Debug
+    del timestamp_rows[123]
+    del timestamp_rows[123]
+    del timestamp_rows[234]
+
+    # Construct and sort dataframe of timestamps
+    df = pd.DataFrame( timestamp_rows, columns=['timestamp'] )
+    df = df.sort_values( by=['timestamp'] )
+
+    # Extract date and find row-to-row differences
+    df['datetime'] = pd.to_datetime( df['timestamp'], unit='ms' )
+    df['date'] = pd.DatetimeIndex( df['datetime'] ).normalize()
+    df['diff'] = df['date'].diff()
+
+    # Remove first row, where difference is irrelevant
+    df = df.iloc[1:]
+
+    # Look for omissions
+    df = df[ df['diff'].ne( '1 days' ) ]
+
+    # Report omissions, if any
+    if len( df ):
+        print( '\n!!! Omissions found in database !!!\n' )
+        df = df[ [ 'date', 'diff' ] ]
+        df = df.rename( index=str, columns={ 'date': 'Ending at', 'diff': 'Interval between readings' } )
+        print( df.to_string( index=False ) )
+
+    return
+
+
 if __name__ == '__main__':
 
     # Get hostname and port of BACnet Gateway, and name of input CSV file
@@ -54,7 +95,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Open the database
-    common.open_db( remove=args.remove )
+    conn, cur = common.open_db( remove=args.remove )
 
     # Save timestamp of this operation
     timestamp_id = common.save_timestamp()
@@ -66,6 +107,9 @@ if __name__ == '__main__':
 
         for baselines_row in reader:
             save_baselines( baselines_row )
+
+    # Report missing dates, from the first database entry until today
+    report_missing_dates()
 
     # Commit changes
     common.commit()
