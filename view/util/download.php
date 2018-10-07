e2aa26a8df1ecb51993c4fff00b6cbc0518e1b59
navkal <navkal@hotmail.com>
2018-10-07 14:28:41 -0400

    Download

--- view/util/download.php
+++ view/util/download.php
@@ -9,86 +9,93 @@
   {
     $g_sCsvBasename = $_REQUEST['csv_basename'];
 
-    // Get view description and cached data
-    chdir( $_SERVER['DOCUMENT_ROOT'] );
-    include $_SERVER['DOCUMENT_ROOT'].'/view/layout/' . $_REQUEST['csv_basename'] . '.php';
-    include $_SERVER['DOCUMENT_ROOT'].'/view/util/common.php';
+    $sLayoutScript = $_SERVER['DOCUMENT_ROOT'].'/view/layout/' . $g_sCsvBasename . '.php';
 
-    // Use view description and cached data to build spreadsheet
-    $g_aCachedValues = (array) $g_tCachedValues;
-    foreach ( $aLines as $sKey => $aLine )
+    if ( file_exists( $sLayoutScript ) )
     {
-      $sLabel = array_shift( $aLine );
-      $sFacility = array_shift( $aLine );
-      $aInstances = $aLine;
-      error_log( '====> Line=' . $sLabel . ' ' . $sFacility . ' ' . print_r( $aInstances, true ) );
+      // Get layout parameters
+      include $sLayoutScript;
 
-      if ( isset( $g_aCachedValues[$sFacility] ) )
+      // Get view description and cached data
+      chdir( $_SERVER['DOCUMENT_ROOT'] );
+      include $_SERVER['DOCUMENT_ROOT'].'/view/util/common.php';
+
+      // Use view description and cached data to build spreadsheet
+      $g_aCachedValues = (array) $g_tCachedValues;
+      foreach ( $aLines as $sKey => $aLine )
       {
-        $aFacility = (array) $g_aCachedValues[$sFacility];
-        $sTest = '--> ' . $sLabel. ':';
-        foreach ( $aInstances as $iOffset => $iInstance )
+        $sLabel = array_shift( $aLine );
+        $sFacility = array_shift( $aLine );
+        $aInstances = $aLine;
+        error_log( '====> Line=' . $sLabel . ' ' . $sFacility . ' ' . print_r( $aInstances, true ) );
+
+        if ( isset( $g_aCachedValues[$sFacility] ) )
         {
-          if ( isset( $aFacility[$iInstance] ) )
+          $aFacility = (array) $g_aCachedValues[$sFacility];
+          $sTest = '--> ' . $sLabel. ':';
+          foreach ( $aInstances as $iOffset => $iInstance )
           {
-            $aData = (array) $aFacility[$iInstance];
-            $aColNames = (array) $g_aColNames[$iOffset];
-            $sTest .= ' ' . $aColNames['value_col_name'] . '=' . $aData[$aData['property']] . ' ' . $aColNames['units_col_name'] . '=' . $aData['units'];
+            if ( isset( $aFacility[$iInstance] ) )
+            {
+              $aData = (array) $aFacility[$iInstance];
+              $aColNames = (array) $g_aColNames[$iOffset];
+              $sTest .= ' ' . $aColNames['value_col_name'] . '=' . $aData[$aData['property']] . ' ' . $aColNames['units_col_name'] . '=' . $aData['units'];
+            }
+          }
+          error_log( $sTest );
+        }
+      }
+
+      // Extract the data into arrays of columns and rows
+      $aColumns = [];
+      $aRows = [];
+      foreach ( $g_tCachedValues as $tData )
+      {
+        foreach ( $tData as $tRow )
+        {
+          array_push( $aRows, (array) $tRow );
+
+          foreach ( $tRow as $sColumn => $sCell )
+          {
+            $aColumns[$sColumn] = $sColumn;
           }
         }
-        error_log( $sTest );
       }
-    }
 
-    // Extract the data into arrays of columns and rows
-    $aColumns = [];
-    $aRows = [];
-    foreach ( $g_tCachedValues as $tData )
-    {
-      foreach ( $tData as $tRow )
+      //
+      // Generate CSV file representing current view
+      //
+
+      // Open the file
+      $sPath = sys_get_temp_dir() . '/' . $g_sCsvBasename . '_' . uniqid() . '.csv';
+      $tFile = fopen( $sPath, 'w' );
+
+      // Write column headers to the file
+      sort( $aColumns );
+      fputcsv( $tFile, $aColumns );
+
+      // Write rows to the file
+      foreach ( $aRows as $aRow )
       {
-        array_push( $aRows, (array) $tRow );
-
-        foreach ( $tRow as $sColumn => $sCell )
+        // Build row of cells
+        $aCells = [];
+        foreach ( $aColumns as $sColumn )
         {
-          $aColumns[$sColumn] = $sColumn;
+          $aCells[$sColumn] = ( isset( $aRow[$sColumn] ) ) ? ( ( strpos( $aRow[$sColumn], ',' ) === false ) ? $aRow[$sColumn] : quote( $aRow[$sColumn] ) ) : '';;
         }
-      }
-    }
 
-    //
-    // Generate CSV file representing current view
-    //
-
-    // Open the file
-    $sPath = sys_get_temp_dir() . '/' . $g_sCsvBasename . '_' . uniqid() . '.csv';
-    $tFile = fopen( $sPath, 'w' );
-
-    // Write column headers to the file
-    sort( $aColumns );
-    fputcsv( $tFile, $aColumns );
-
-    // Write rows to the file
-    foreach ( $aRows as $aRow )
-    {
-      // Build row of cells
-      $aCells = [];
-      foreach ( $aColumns as $sColumn )
-      {
-        $aCells[$sColumn] = ( isset( $aRow[$sColumn] ) ) ? ( ( strpos( $aRow[$sColumn], ',' ) === false ) ? $aRow[$sColumn] : quote( $aRow[$sColumn] ) ) : '';;
+        // Write row to the file
+        fputcsv( $tFile, $aCells );
       }
 
-      // Write row to the file
-      fputcsv( $tFile, $aCells );
+      // Close the file
+      fclose( $tFile );
+
+      // Download the file
+      downloadFile( $sPath );
+
+      // Delete the file
+      unlink( $sPath );
     }
-
-    // Close the file
-    fclose( $tFile );
-
-    // Download the file
-    downloadFile( $sPath );
-
-    // Delete the file
-    unlink( $sPath );
   }
 ?>
