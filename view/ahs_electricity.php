a4d2318814c2480a0b13c7c6b5a750e0ba7cdd90
navkal <navkal@hotmail.com>
2018-10-07 13:30:50 -0400

    Download

--- view/ahs_electricity.php
+++ view/ahs_electricity.php
@@ -1,6 +1,6 @@
 <?php
   // Copyright 2018 BACnet Gateway.  All rights reserved.
 
-  include $_SERVER['DOCUMENT_ROOT'] . '/view/layout/ahs_electricity.php';
+  include $_SERVER['DOCUMENT_ROOT'] . '/view/layout/' . basename( __FILE__, '.php' ) . '.php';
   include $_SERVER['DOCUMENT_ROOT'] . '/view/util/view.php';
 ?>
--- view/dishboard.php
+++ view/dishboard.php
@@ -1,6 +1,6 @@
 <?php
   // Copyright 2018 BACnet Gateway.  All rights reserved.
 
-  include $_SERVER['DOCUMENT_ROOT'] . '/view/layout/dishboard.php';
+  include $_SERVER['DOCUMENT_ROOT'] . '/view/layout/' . basename( __FILE__, '.php' ) . '.php';
   include $_SERVER['DOCUMENT_ROOT'] . '/view/util/view.php';
 ?>
