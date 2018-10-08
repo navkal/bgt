2191a83a3efbdfd42b1a4202ded123b52e20a11f
navkal <navkal@hotmail.com>
2018-10-07 23:34:01 -0400

    Download

--- view/util/download.php
+++ view/util/download.php
@@ -20,10 +20,14 @@
       chdir( $_SERVER['DOCUMENT_ROOT'] );
       include $_SERVER['DOCUMENT_ROOT'].'/view/util/common.php';
 
-      // Use view description and cached data to build spreadsheet
-      $aColumns = [];
+      //
+      // Build spreadsheet
+      //
+
+      $aHead = [];
       $aRows = [];
       $g_aCachedValues = (array) $g_tCachedValues;
+
       foreach ( $aLines as $sKey => $aLine )
       {
         $sLabel = array_shift( $aLine );
@@ -43,10 +47,10 @@
               $aData = (array) $aFacility[$iInstance];
               $aColNames = (array) $g_aColNames[$iOffset];
               $sTest .= ' ' . $aColNames['value_col_name'] . '=' . $aData[$aData['property']] . ' ' . $aColNames['units_col_name'] . '=' . $aData['units'];
-              if ( count( $aColumns ) < count( $aInstances ) * 2 )
+              if ( count( $aHead ) < count( $aInstances ) * 2 )
               {
-                array_push( $aColumns, $aColNames['value_col_name'] );
-                array_push( $aColumns, $aColNames['units_col_name'] );
+                array_push( $aHead, $aColNames['value_col_name'] );
+                array_push( $aHead, $aColNames['units_col_name'] );
               }
               array_push( $aRow, $aData[$aData['property']] );
               array_push( $aRow, $aData['units'] );
@@ -59,30 +63,14 @@
       }
 
       // Add first and last column names
-      array_unshift( $aColumns, $g_sFirstColName );
-      array_push( $aColumns, UPDATE_TIME );
+      array_unshift( $aHead, $g_sFirstColName );
+      array_push( $aHead, UPDATE_TIME );
 
-      error_log( '===============> columns=' . print_r( $aColumns, true ) );
+      error_log( '===============> columns=' . print_r( $aHead, true ) );
 
-
-      // Extract the data into arrays of columns and rows
-      $aColumns = [];
-      $aRows = [];
-      foreach ( $g_tCachedValues as $tData )
-      {
-        foreach ( $tData as $tRow )
-        {
-          array_push( $aRows, (array) $tRow );
-
-          foreach ( $tRow as $sColumn => $sCell )
-          {
-            $aColumns[$sColumn] = $sColumn;
-          }
-        }
-      }
 
       //
-      // Generate CSV file representing current view
+      // Dump spreadsheet to CSV file
       //
 
       // Open the file
@@ -90,21 +78,12 @@
       $tFile = fopen( $sPath, 'w' );
 
       // Write column headers to the file
-      sort( $aColumns );
-      fputcsv( $tFile, $aColumns );
+      fputcsv( $tFile, $aHead );
 
       // Write rows to the file
       foreach ( $aRows as $aRow )
       {
-        // Build row of cells
-        $aCells = [];
-        foreach ( $aColumns as $sColumn )
-        {
-          $aCells[$sColumn] = ( isset( $aRow[$sColumn] ) ) ? ( ( strpos( $aRow[$sColumn], ',' ) === false ) ? $aRow[$sColumn] : quote( $aRow[$sColumn] ) ) : '';;
-        }
-
-        // Write row to the file
-        fputcsv( $tFile, $aCells );
+        fputcsv( $tFile, $aRow );
       }
 
       // Close the file
