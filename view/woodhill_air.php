97749ea4264831b66bf78cfd92174283c5edd851
navkal <navkal@hotmail.com>
2018-10-19 11:32:28 -0400

    Initialize table before splitter

---
+++ view/woodhill_air.php
@@ -0,0 +1,21 @@
+<?php
+  // Copyright 2018 BACnet Gateway.  All rights reserved.
+
+  $g_sLayoutMode = $g_sLayoutModeDefault;
+
+  $g_sCsvFilename = 'csv/woodhill_air.csv';
+  $g_sFirstColName = 'Location';
+  $g_aColNames =
+  [
+    [
+      'value_col_name' => 'Temperature',
+      'units_col_name' => 'Temperature Units',
+      'graph' =>
+      [
+        'graph_id' => 'temperature'
+      ]
+    ]
+  ];
+
+  include $_SERVER['DOCUMENT_ROOT'] . '/view/util/view.php';
+?>
