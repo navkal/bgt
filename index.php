9e0e2daced7a6de2ec10bbc90cc84e990b4544c0
navkal <navkal@hotmail.com>
2018-10-19 15:50:40 -0400

    Migrate from Bootstrap 3 to Bootstrap 4

--- index.php
+++ index.php
@@ -23,10 +23,10 @@
   // Load jQuery library outside the document head.  split.js needs this; don't know why.
   define(
     'BOOTSTRAP_VERSION',
-    /******************************** /
-    '_4'
     /********************************/
     ''
+    /******************************** /
+    '_4'
     /********************************/
   );
   require_once '../common/libraries' . BOOTSTRAP_VERSION . '.php';  // <-- Includes jQuery, redundant with document head; needed by split.js (don't know why)
