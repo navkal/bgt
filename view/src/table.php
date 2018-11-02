984145b42659720219f67dc4d43e95b805b877f2
navkal <navkal@hotmail.com>
2018-11-01 23:03:37 -0400

    Show title above table

--- view/src/table.php
+++ view/src/table.php
@@ -1,19 +1,29 @@
 <?php
   // Copyright 2018 Building Monitor.  All rights reserved.
+
+  global $navbarItems;
+  global $navbarItemKey;
 ?>
 
 <table id="bgt_table" class="table" style="display:none" >
 
   <thead>
 
+    <!-- Title -->
+    <tr>
+      <th class="sorter-false tablesorter-headerRow" colspan="<?=2 + ( count( $g_aColNames ) * 2 )?>" >
+        <span class="btn btn-sm" style="cursor:default; font-size:0.9375rem" >
+          <?=$navbarItems[$navbarItemKey][0]?>
+        </span>
+        <span class="btn btn-sm float-right" style="cursor:default;" >
+          <span id="bgt_table_row_count"></span> row<span id="bgt_table_row_count_s">s</span>
+        </span>
+      </th>
+    </tr>
+
     <!-- Toolbar -->
     <tr>
       <th class="sorter-false tablesorter-headerRow" colspan="<?=2 + ( count( $g_aColNames ) * 2 )?>" >
-        <span class="float-left">
-            <span class="btn btn-sm" style="color:#2281cf;" >
-              <span id="bgt_table_row_count"></span> row<span id="bgt_table_row_count_s">s</span>
-            </span>
-        </span>
         <span class="float-right">
           <button id="refreshButton" class="btn btn-sm btn-link" onclick="toggleRefresh()" disabled >
             <i id="startRefreshIcon" class="fas fa-sync-alt text-success"></i>
