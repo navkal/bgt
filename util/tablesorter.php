2668a64250f38269973dc282ce9537e378518fdc
navkal <navkal@hotmail.com>
2018-11-06 22:09:43 -0500

    Refine view initialization

--- util/tablesorter.php
+++ util/tablesorter.php
@@ -50,8 +50,8 @@ var g_tTableProps =
 </script>
 
 <!-- tablesorter theme -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.29.0/css/theme.dropbox.min.css" integrity="sha256-VFOuP1wPK9H/EeQZEmYL0TZlkMtUthqMBdrqfopliF4=" crossorigin="anonymous" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.0/css/theme.dropbox.min.css" integrity="sha256-CbNE7knbzUGwr4jEImul6Ww8oP32d5W88KjDPoJUzdk=" crossorigin="anonymous" />
 
 <!-- tablesorter basic libraries -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.29.0/js/jquery.tablesorter.min.js" integrity="sha256-UD/M/6ixbHIPJ/hTwhb9IXbHG2nZSiB97b4BSSAVm6o=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.29.0/js/jquery.tablesorter.widgets.min.js" integrity="sha256-/3WKCLORjkqCd7cddzHbnXGR31qqys81XQe2khfPvTY=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.0/js/jquery.tablesorter.min.js" integrity="sha256-Ae7jmRrbL3hf1J/y22SYMPtx0wMVbG4g3HtpjioYuyk=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.0/js/jquery.tablesorter.widgets.js" integrity="sha256-sg80NyaLmey6oXCdI+VhKtRMYkI//IMuua1N9pG9HI8=" crossorigin="anonymous"></script>
